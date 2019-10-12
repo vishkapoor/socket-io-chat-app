2097891ab82582abc91ee6d2e072852ccad62923
Vishal Kapoor <vishal.kapoor@ecdltd.com>
2019-10-12 10:06:31 +0100

    vujs and bootstrap integrated

--- index.js
+++ index.js
@@ -6,13 +6,14 @@ app.get('/', function(req, res) {
     res.sendFile(__dirname + '/index.html');
 });
 
-http.listen(3000, function() {
+http.listen(3000, () => {
   console.log('listening on *:3000');
 });
 
 io.on('connection', function(socket) {
-    console.log('a user connected');
-    socket.on('disconnect', function() {
-        console.log('user disconnected.');
+    socket.on('created', (data) => {
+        console.log(data);
+       socket.broadcast.emit('created', (data));
     });
+
 });
